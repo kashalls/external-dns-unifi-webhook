b9ce52e902f8ecbf89c15290d1270f31429f286e
Devin Buhl <devin@buhl.casa>
2024-05-24 16:46:53 -0400

    fix: make unifi env vars required
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- internal/unifi/configuration.go
+++ internal/unifi/configuration.go
@@ -2,8 +2,8 @@ package unifi
 
 // Configuration holds configuration from environmental variables
 type Configuration struct {
-	Host          string `env:"UNIFI_HOST"`
-	User          string `env:"UNIFI_USER" envDefault:"external-dns-unifi"`
-	Password      string `env:"UNIFI_PASS" envDefault:"V3ryS3cur3!!"`
+	Host          string `env:"UNIFI_HOST,required"`
+	User          string `env:"UNIFI_USER,required"`
+	Password      string `env:"UNIFI_PASS,required"`
 	SkipTLSVerify bool   `env:"UNIFI_SKIP_TLS_VERIFY" envDefault:"false"`
 }
